e6abb73cdee79d66c45bbed021dd61e9061332d8
axelajv <axelajavon@gmail.com>
2015-03-05 18:59:34 +0100

    Affichage dynamique ligne de métro
    -Affichage dynamique d’une ligne de métro en fonction du choix
    renseigné dans la barre de recherche

--- js/source-data.js
+++ js/source-data.js
@@ -1,3 +1,31 @@
+// Affiche le plan du métro en fonction de la ligne renseigné
+function affiche_metro(ligne){
+
+  //On cache la barre de recherche
+  var searchBar = document.getElementById('searchBar');
+  searchBar.style.display = "none"; 
+
+  // On récupuère la div du titre et on le modifie
+  var titre = document.getElementById('titre-ligne');
+  titre.firstChild.data = "Ligne "+ligne
+  
+  //On affiche le titre
+  titre.style.display = "block";
+
+  //On récupère le lien de l'image et on le modifie
+  var div_image = document.getElementById("image-metro");
+  div_image.style.display = "block";
+
+  var image = document.getElementById("image-metro2");
+  image.setAttribute('src', 'images/metro-ligne-'+ligne+'.gif');
+
+  //On affiche le bouton précédent
+  var button_return = document.getElementById('button_return');
+  button_return.style.display = "block";
+
+}
+
+
 $(function() {
     var availableTags = [
       "RER A",
@@ -22,7 +50,9 @@ $(function() {
       "Metro 13",
       "Metro 14"
     ];
-    $( "#recherche-ligne" ).autocomplete({
+    $( "#autocomplete" ).autocomplete({
+      delay: 100,
+
       source: availableTags,
 
       messages: {
@@ -32,13 +62,68 @@ $(function() {
 
     select : function(event, ui){
 
-        //alert( ui.item.value );
+        // prevent autocomplete from updating the textbox
+        event.preventDefault();
+       
 
         var metro = ui.item.value;
 
         if ( metro=="Metro 1")
         {
-          window.location.replace("affiche-metro.html");
+          affiche_metro(1);        
+        }
+        else if ( metro=="Metro 2")
+        {
+          affiche_metro(2);
+          
+        }
+        else if ( metro=="Metro 3")
+        {
+          affiche_metro(3); 
+        }
+        else if ( metro=="Metro 4")
+        {
+          affiche_metro(4);
+        }
+        else if ( metro=="Metro 5")
+        {
+          affiche_metro(5);
+        }
+        else if ( metro=="Metro 6")
+        {
+          affiche_metro(6);
+        }
+        else if ( metro=="Metro 7")
+        {
+         affiche_metro(7); 
+        }
+        else if ( metro=="Metro 8")
+        {
+         affiche_metro(8); 
+        }
+        else if ( metro=="Metro 9")
+        {
+         affiche_metro(9); 
+        }
+        else if ( metro=="Metro 10")
+        {
+         affiche_metro(10); 
+        }
+        else if ( metro=="Metro 11")
+        {
+         affiche_metro(11); 
+        }
+        else if ( metro=="Metro 12")
+        {
+         affiche_metro(12); 
+        }
+        else if ( metro=="Metro 13")
+        {
+         affiche_metro(13); 
+        }
+        else if ( metro=="Metro 14")
+        {
+         affiche_metro(14); 
         }
         
         //alert( ui.item.value ); // lance une alerte indiquant la valeur de la proposition
